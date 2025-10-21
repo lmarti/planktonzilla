68b407a0ec4c564cd3efc1b5d676881089fb3766
Luis Martí <lmarti@users.noreply.github.com>
2025-10-21 16:34:10 -0300

    adding train_params to propagate to train_node

--- planktonzilla/train_node.py
+++ planktonzilla/train_node.py
@@ -1,15 +1,18 @@
 """
-Prueba de ejecución Hydra + Submitit (Jean Zay)
+(c) Inria
 """
 
-import os
-import socket
-import datetime
-import pyrootutils
-from omegaconf import DictConfig
 import hydra
+import pyrootutils
+from hydra.core.hydra_config import HydraConfig
+from hydra.types import RunMode
+from omegaconf import DictConfig
 
-# --- Configuración raíz del proyecto ---
+from planktonzilla.utils.logger import get_pylogger
+
+log = get_pylogger(__name__)
+
+
 root = pyrootutils.setup_root(
     search_from=__file__,
     indicator=[".git", "pyproject.toml"],
@@ -17,10 +20,13 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 
-@hydra.main(version_base="1.3", config_path=str(root / "configs"), config_name="train.yaml")
+@hydra.main(version_base="1.3", config_path=str(root / "configs"), config_name="train_node.yaml")
 def main(cfg: DictConfig):
-    print("\n" + "AAAAAAAAA")
-    print("\n" + "AAAAAAAAA")
+    if HydraConfig.get().mode != RunMode.MULTIRUN:
+        log.error(f"Run mode is {HydraConfig.get().mode}, while RunMode.MULTIRUN was expected.")
+        log.error("Command is meant launch an hydra multirun that launches experiments in SLURM clusters with hydra.")
+    else:
+        log.info(f"Success with params: {cfg.train_param}.")
 
 
 if __name__ == "__main__":
